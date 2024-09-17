7614f40bf8616ab2cb6bbe6f538f92f9166f7b55
Thomas Hagen <43868840+ironmagen@users.noreply.github.com>
2024-09-16 21:23:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -28,10 +28,6 @@ def add_item():
     inventory_items_table.add_item(item_data)
     return redirect(url_for('get_inventory'))
 
-# Inventory Input Routes
-from inventory_input import InventoryInput
-inventory_input = InventoryInput()
-
 @app.route('/input', methods=['GET'])
 def get_input():
     input_data = inventory_input.get_input()
@@ -42,10 +38,6 @@ def submit_input():
     input_data = request.form
     inventory_input.submit_input(input_data)
     return redirect(url_for('get_input'))
-
-# Inventory Utilities Routes
-from inventory_utilities import InventoryUtilities
-inventory_utilities = InventoryUtilities()
 
 @app.route('/utilities', methods=['GET'])
 def get_utilities():
@@ -58,10 +50,6 @@ def run_utility():
     inventory_utilities.run_utility(utility_data)
     return redirect(url_for('get_utilities'))
 
-# Sales Items Routes
-from sales_items import SalesItems
-sales_items = SalesItems()
-
 @app.route('/sales', methods=['GET'])
 def get_sales():
     sales_data = sales_items.get_sales()
@@ -72,10 +60,6 @@ def add_sale():
     sale_data = request.form
     sales_items.add_sale(sale_data)
     return redirect(url_for('get_sales'))
-
-# Orders Routes
-from orders import Orders
-orders = Orders()
 
 @app.route('/orders', methods=['GET'])
 def get_orders():
@@ -88,10 +72,6 @@ def place_order():
     orders.place_order(order_data)
     return redirect(url_for('get_orders'))
 
-# Deliveries Routes
-from deliveries import Deliveries
-deliveries = Deliveries()
-
 @app.route('/deliveries', methods=['GET'])
 def get_deliveries():
     deliveries_data = deliveries.get_deliveries()
@@ -99,8 +79,18 @@ def get_deliveries():
 
 @app.route('/record_delivery', methods=['POST'])
 def record_delivery():
-    delivery_data = request.form
-    deliveries.record_delivery(delivery_data)
+    order_id = request.form['order_id']
+    date = request.form['date']
+    status = request.form['status']
+
+    # Validate data
+    if not order_id or not date or not status:
+        return 'Error: Please fill out all fields.'
+
+    # Store data in database
+    deliveries.add_delivery(order_id, date, status)
+
+    # Return success message
     return redirect(url_for('get_deliveries'))
 
 if __name__ == '__main__':
